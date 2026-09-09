f0093cf340ded9bf5268d1f487574a8489ed6572
starrysky88 <starrysky10329@gmail.com>
2026-09-09 11:56:44 +0800

    week01

---
+++ week01/week01-3.cpp
@@ -0,0 +1,13 @@
+//week01-3.cpp學習計畫Basic第3題
+//LeetCode 28. Find the Index of the First Occurrence in a String
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int N1 = haystack.length(), N2 = needle.length(); //函式呼叫，字串長度
+        for(int i=0;i< N1-N2;i++){
+            if(haystack.substr(i,N2) == needle) return i;
+            //如果大字串的.substr(開始,長度)等於小字串，就找到答案了
+        }
+        return -1; //找不到
+    }
+};
